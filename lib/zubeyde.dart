35d8f09eb38d1150739275c9905fb59776ff38ee
b2200356814 <b2200356814@cs.hacettepe.edu.tr>
2022-07-06 12:37:29 +0300

    Update zubeyde.dart

---
+++ lib/zubeyde.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Zubeyde extends StatefulWidget {
+  const Zubeyde({Key? key}) : super(key: key);
+
+  @override
+  State<Zubeyde> createState() => _ZubeydeState();
+}
+
+class _ZubeydeState extends State<Zubeyde> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
