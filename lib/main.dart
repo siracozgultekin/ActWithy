938889af734f86db9efa19113955919825815fe6
Sirac Özgültekin <73199372+siracozgultekin@users.noreply.github.com>
2022-07-07 00:22:32 +0300

    FirebaseAuth

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:actwithy/pages/welcomePage.dart';
 import 'package:flutter/material.dart';
-void main(){
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+Future<void> main()async{
+  await WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 class MyApp extends StatefulWidget {
--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -1,35 +1,35 @@
+import 'package:actwithy/pages/homePage.dart';
+import 'package:actwithy/pages/welcomePage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthService{
   String uid='';
-  Future<bool> register(String email, String pass, String username, String name) async {
+  Future<bool> register(String email, String pass, String username, String name,String surname) async {
     bool result = false;
     print('Register basarili...');
 
     await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email, password: pass)
         .then((value) async {
-      uid = value.user!.uid;
-      List<String> followers =[];
-      List<String> following =[];
+      String uid = value.user!.uid;
+      List<String> friends =[];
+      List<String> posts =[];
+
       Map<String, dynamic> usermap = {
-        "userId": uid,
+        "userUID": uid,
         "email": email,
         "password":pass,
-
-        "kullaniciadi": username,
         "name": name,
-        "olusturmaTarihi": Timestamp.now(),
-
-
-        "follower": 0,
-        "following": 0,
+        "surname":surname,
+        "kullaniciadi": username,
         "bio": "bio",
-        "userBackgroundPhoto": "picUrl",
-        "userPicUrl": "picUrl",
-        "followersId" : followers,
-        "followingId" : following,
+        "ppURL": "ppUrl",
+        "backgroundURL": "bgUrl",
+        "friends":friends,
+        "posts": posts,
+        "registerDate": Timestamp.now(),
+        "lastSeen": Timestamp.now(),
       };
       await FirebaseFirestore.instance.collection('users').doc(uid).set(usermap)
           .onError((error, stackTrace) {
@@ -40,6 +40,37 @@ class AuthService{
       result = false;
     });
     return result;
+  }
+  Future<void> signIn(String email, String pass, BuildContext context) async {
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: pass)
+        .then((value) {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+    }).onError((error, stackTrace) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(snacks("Hata oluştu. Tekrar deneyiniz"));
+    });
+  }
+
+  Future<void> signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut().then((value) {
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (context) => WelcomePage()));
+    }).onError((error, stackTrace) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(snacks("Hata oluştu. Daha sonra tekrar deneyiniz"));
+    });
+  }
+
+  SnackBar snacks(String text) {
+    return SnackBar(
+      content: Text(text),
+      action: SnackBarAction(
+        label: "X",
+        onPressed: () {},
+      ),
+    );
   }
 }
 
