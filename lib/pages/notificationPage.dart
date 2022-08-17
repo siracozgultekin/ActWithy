79b8cac0480e6f414148015966cf547321d35c38
Sirac Özgültekin <ozgultekin.sirac@hotmail.com>
2022-08-17 15:08:50 +0300

    navigator setstates

--- lib/pages/notificationPage.dart
+++ lib/pages/notificationPage.dart
@@ -88,23 +88,18 @@ class _NotificationPageState extends State<NotificationPage> {
 
             setState(() {
               selectedIndex = value;
-
-              switch(selectedIndex){
-                case 0: Navigator.of(context).pop();
-                break;
-                case 1: showSearch(context: context, delegate: SearchPage(hintText: "Search", hintTextColor: TextStyle(color: Colors.white)));
-                break;
-                case 2: Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => CreatingPage(postModel: postModel)));
-                break;
-                case 4: Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ProfilePage(user: currentUser)));
-                break;
-
-              }
-
-
-
-
             });
+            switch(selectedIndex){
+              case 0: Navigator.of(context).pop();
+              break;
+              case 1: showSearch(context: context, delegate: SearchPage(hintText: "Search", hintTextColor: TextStyle(color: Colors.white)));
+              break;
+              case 2: Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => CreatingPage(postModel: postModel)));
+              break;
+              case 4: Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ProfilePage(user: currentUser)));
+              break;
+
+            }
           },
           destinations: [
             NavigationDestination(
