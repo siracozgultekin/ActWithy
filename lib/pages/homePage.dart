31a311edd6dd52d66f2d319ef2780cb96567ecb2
zubeydecivelek <zubeydeecivelek@gmail.com>
2022-08-19 12:24:13 +0300

    Update homePage.dart

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -33,6 +33,7 @@ class _HomePageState extends State<HomePage> {
   List<bool> isReaction = [];
   List<List<int>> amIparticipateList = [];
   List<List<bool>> isRequest = [];
+  bool destinationClicked =false;
 
   @override
   void initState() {
@@ -149,7 +150,8 @@ class _HomePageState extends State<HomePage> {
         ),
         child: NavigationBar(
           selectedIndex: selectedIndex,
-          onDestinationSelected: (value) async {
+          onDestinationSelected:destinationClicked? (value){}: (value) async {
+            setState((){destinationClicked=true;});
             bool check = await PostServices().checkDailyPost();
 
             PostModel postModel;
@@ -187,6 +189,7 @@ class _HomePageState extends State<HomePage> {
               break;
 
             }
+            setState((){destinationClicked=false;});
           },
           destinations: [
             NavigationDestination(
